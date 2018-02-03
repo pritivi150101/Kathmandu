f830c50037abd0fc4d9e12e91abc604f1a121cfb
pritivi150101 <33313124+pritivi150101@users.noreply.github.com>
2018-02-03 09:52:53 +0530

    Update Contents.swift
    Brightness function for each individual pixel added

--- Kathmandu.playground/Contents.swift
+++ Kathmandu.playground/Contents.swift
@@ -1,5 +1,10 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
-
 var str = "Hello, playground"
+func Bright(pix:Pixel, intensity:Int)-> Pixel{
+    let multiplier = 1-(intensity/10)
+    var newpix: Pixel=pix
+    newpix.red=UInt8(Int(pix.red) * multiplier)
+    newpix.green=UInt8(Int(pix.green) * multiplier)
+    newpix.blue=UInt8(Int(pix.blue) * multiplier)
+    return newpix
+}
